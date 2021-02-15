55120d53afd80e1d2388060301905713a7d39b90
FrozenKub <simachev.anton@mail.ru>
2021-02-15 15:57:16 +0300

    DP + RMR added

--- src/sights-nvd.jsx
+++ src/sights-nvd.jsx
@@ -5,8 +5,8 @@ class SightsNVD extends React.Component{
     constructor(props) {
         super(props);
 
-
         this.state = {first: "", second: ""}
+        this.imgline1= JSON.stringify("/" + this.state.first + this.state.second + "1.jpg");
     }
 
     handleSecondClick(e)
@@ -47,7 +47,7 @@ render()
                         <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "DP"})}}>Leupold DeltaPoint Reflex Sight</button>
                         <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "RMR"})}}>Trijicon RMR</button>
                         <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "SR4"})}}>	Sig Sauer Romeo 4 reflex sight	</button>
-                        <button onClick={e=>{console.log(this.state.first, " ", this.state.second)}}>	CONSOLE	</button>
+                        <button onClick={e=>{console.log(this.state.first, " ", this.state.second, " ", JSON.stringify("/" + this.state.first + this.state.second + "2.jpg")) }}>	CONSOLE	</button>
                     </div>
 
                     <div className="third-block" id="third-block">
@@ -56,27 +56,12 @@ render()
                             <h1>{this.state.second}</h1>
                         </div>
                         <div className="third-block gif-block">
-                            <img src="https://i.imgur.com/fsvqsUa.png" className="third-block gif-block pic" alt="Left Pic"/>
-                            <img src="https://i.imgur.com/2vN2nr8.jpg" className="third-block gif-block  pic" alt="Right Pic"/>
+                            <img src={'/' + this.state.first + this.state.second + '1.jpg'} className="third-block gif-block pic" alt="Left Pic"/>
+                            <img src={'/' + this.state.first + this.state.second + '2.jpg'} className="third-block gif-block  pic" alt="Right Pic"/>
                         </div>
                     </div>
 
                 </div>
-
-
-            <div className="centered">
-                <a href="/root">
-                    <img src="https://upload.wikimedia.org/wikipedia/ru/7/7a/Escape_from_Tarkov_logo.png" className="main-page-logo-upper" alt="Logo"/>
-                </a>
-
-                <div className="full-screen">
-                        <video src="/eft_back.mp4" autoPlay muted loop></video>
-
-
-
-
-                </div>
-            </div>
     </>
         );
     }
