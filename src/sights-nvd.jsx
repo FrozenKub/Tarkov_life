4719afe3d6eac884ed27d03d654f008d5959be03
frozenkub <simachev.anton@mail.ru>
2021-03-11 09:37:02 +0300

    Fixing second buttons

--- src/sights-nvd.jsx
+++ src/sights-nvd.jsx
@@ -6,7 +6,7 @@ class SightsNVD extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state = {first: "", second: "", second_third: "second-third", second_block: "second-block", third_block: "third-block", second_pic: "second-pic", first_pic: "first-pic", update: "updated", crs: "auf", cmp: ""}
+        this.state = {first: "", second: "", second_third: "second-third", second_block: "second-block", third_block: "third-block", second_pic: "second-pic", first_pic: "first-pic", update: "updated", menu: ""}
         this.imgline1= JSON.stringify("/" + this.state.first + this.state.second + "1.jpg");
     }
 
@@ -38,15 +38,18 @@ class SightsNVD extends React.Component{
 
     handleOpen(e, menu)
     {
-        e.preventDefault()
-        if (this.state.menu === "lighter-button")
+        if (document.getElementById(menu).style.display == "grid")
         {
-            this.state.menu = "lighter-button-pressed"
+            document.getElementById(menu+ "_BUT").style.color = "#777667";
+            document.getElementById(menu+ "_BUT").style.backgroundColor = "#00000000";
+            document.getElementById(menu).style.display = "none";
         }
-        else this.state.menu = "lighter-button"
-
-        this.setState({update: "updated"})
-
+        else
+        {
+            document.getElementById(menu+ "_BUT").style.color = "black";
+            document.getElementById(menu+ "_BUT").style.backgroundColor = "#777667";
+            document.getElementById(menu).style.display = "grid";
+        }
     }
 
     render()
@@ -68,7 +71,7 @@ class SightsNVD extends React.Component{
                         </div>
                         <div className={this.state.second_third} id="second-third-block">
                         <div className={this.state.second_block} id="second-block">
-                                <button className={this.state.crs} id="CRS_BUT" onClick={e=>{this.handleOpen(e, "crs");}}> COMPACT REFLEX SIGHTS</button>
+                                <button className="lighter-button" id="CRS_BUT" onClick={e=>{this.handleOpen(e, "CRS");}}> COMPACT REFLEX SIGHTS</button>
                             <div id="CRS" className="menu-pop">
                                 <button id="CRS" onClick={e=>{this.handleThirdClick(e); this.setState({second: "PK6"})}}>PK-06<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/6/62/Pk-06icon.png" className="small-pic"/></button>
                                 <button id="CRS" onClick={e=>{this.handleThirdClick(e); this.setState({second: "T1"})}}>T-1<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/2/20/T-1icon.png" className="small-pic"/></button>
@@ -79,7 +82,7 @@ class SightsNVD extends React.Component{
                             </div>
 
 
-                            <button className={this.state.lighter_button} id="RS_BUT" onClick={e=>{this.handleOpen(e, "RS");}}>REFLEX SIGHTS</button>
+                            <button className="lighter-button" id="RS_BUT" onClick={e=>{this.handleOpen(e, "RS");}}>REFLEX SIGHTS</button>
                             <div id="RS" className="menu-pop">
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "COMP"})}}>COMP M4<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/7/74/Comp_M4_Icon.png" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "EKP02"})}}>EKP-8-02<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/2/20/T-1icon.png" className="small-pic"/></button>
@@ -100,9 +103,9 @@ class SightsNVD extends React.Component{
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "MRS"})}}>MRS<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/0c/Ff3icon.png" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "RING"})}}>Ring<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/7/75/Dpicon.png" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "SRS02"})}}>SRS-02<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/b/bd/Rmricon.png" className="small-pic"/></button>
-                                 </div>
+                            </div>
 
-                                <button className={this.state.lighter_button} id="AS_BUT" onClick={e=>{this.handleOpen(e, "AS");}}> ASSAULT SCOPES</button>
+                                <button className="lighter-button" id="AS_BUT" onClick={e=>{this.handleOpen(e, "AS");}}> ASSAULT SCOPES</button>
                             <div id="AS" className="menu-pop">
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "DR1"})}}>DR1/4x<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/a/aa/SpectreDR_Icon.gif" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "HAMR"})}}>HAMR<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/3/33/Hamricon.png" className="small-pic"/></button>
@@ -113,7 +116,7 @@ class SightsNVD extends React.Component{
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "PS320"})}}>PS320 1x/6x<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/c/c2/Valday_PS-320_icon.png" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "MONSTR"})}}>Monstr. 2x32<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/2/21/Monstrum2x32_Icon.png" className="small-pic"/></button>
                             </div>
-                                <button className={this.state.lighter_button} id="SC_BUT" onClick={e=>{this.handleOpen(e, "SC");}}>SPECIAL SCOPES</button>
+                                <button className="lighter-button" id="SC_BUT" onClick={e=>{this.handleOpen(e, "SC");}}>SPECIAL SCOPES</button>
                             <div id="SC" className="menu-pop">
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "VULCAN"})}}>Vulcan MG 3.5x<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/a/a4/Vulcan_MG_night_scope_3.5x_Icon.png" className="small-pic"/></button>
                                 <button onClick={e=>{this.handleThirdClick(e); this.setState({second: "REAP"})}}>REAP-IR<img src="https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/1/1d/Thermalicon.png" className="small-pic"/></button>
